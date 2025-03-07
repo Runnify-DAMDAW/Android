436be77b2b53cbe8f9c4a7a12a102faf3284f59b
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 19:34:14 +0100

    fix DetalleCarrera

--- app/src/main/java/dam/moviles/runnifydamdaw/vista/DetalleCarreraFragment.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/vista/DetalleCarreraFragment.kt
@@ -1,6 +1,7 @@
 package dam.moviles.runnifydamdaw.vista
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,6 +25,11 @@ class DetalleCarreraFragment : Fragment() {
         binding= FragmentDetalleCarreraBinding.inflate(inflater, container, false)
     }
 
+    private fun actualizarInterfaz(){
+
+
+
+    }
 
 
 }
