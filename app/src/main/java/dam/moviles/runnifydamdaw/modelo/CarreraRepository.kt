35aa07fb5bc0f8aa4993ce723d770cf078b911d7
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 16:29:34 +0100

    Create CarreraRepository.kt

---
+++ app/src/main/java/dam/moviles/runnifydamdaw/modelo/CarreraRepository.kt
@@ -0,0 +1,20 @@
+package dam.moviles.runnifydamdaw.modelo
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+
+class CarreraRepository {
+
+    val carreraApi:CarreraApi
+
+    init {
+        carreraApi = Retrofit.Builder()
+            .baseUrl("http://192.168.70.97:8000")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+    suspend fun consultarTodosActores():List<Carrera> =
+        carreraApi.consultarTodasCarreras()
+}
