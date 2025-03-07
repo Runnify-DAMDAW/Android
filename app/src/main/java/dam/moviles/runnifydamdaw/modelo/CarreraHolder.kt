73f5d22a4d9687a725e1eedafb1d3f4f07ca35bd
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 16:49:23 +0100

    Update CarreraHolder.kt

--- app/src/main/java/dam/moviles/runnifydamdaw/modelo/CarreraHolder.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/modelo/CarreraHolder.kt
@@ -10,6 +10,11 @@ class CarreraHolder(val binding: ItemcarreraBinding) : RecyclerView.ViewHolder(b
     fun mostrarCarrera(c:Carrera){
         carrera = c
         binding.imgCarrera.ponerFotoActor(c)
+        binding.txtNombreCarrera.text = c.name
+        binding.txtTipo.text = c.category
+        binding.txtFecha.text = c.date
+        binding.txtDinero.text = c.entry_fee.toString()
+        binding.txtPlazas.text = c.available_slots.toString()
     }
 
 }
