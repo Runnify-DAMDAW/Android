ac9456e2f92a9881aadfc15d586a8955b3ad9640
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 17:04:08 +0100

    Update MainFragmentViewModel.kt

--- app/src/main/java/dam/moviles/runnifydamdaw/viewModel/MainFragmentViewModel.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/viewModel/MainFragmentViewModel.kt
@@ -2,8 +2,25 @@ package dam.moviles.runnifydamdaw.viewModel
 
 import androidx.lifecycle.ViewModel
 import dam.moviles.runnifydamdaw.modelo.Carrera
+import dam.moviles.runnifydamdaw.modelo.CarreraRepository
 
 class MainFragmentViewModel: ViewModel() {
 
     var listaCarreras:List<Carrera> = emptyList()
+
+
+    suspend fun cargarListaCarreras(
+        lambdaExito: () -> Unit,
+        lambdaError: (String) -> Unit
+    ){
+        try {
+            listaCarreras = CarreraRepository().consultarTodosActores()
+            lambdaExito()
+        }catch (e:Exception){
+            lambdaError(e.message.toString())
+        }
+    }
+
+    fun getNombresCarreras():List<String> =
+        listaCarreras.map { carrera -> carrera.name  }
 }
