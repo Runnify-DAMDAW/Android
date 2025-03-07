b2e3244181af1a01c7895cb82f2d39eb127559d0
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 16:56:44 +0100

    Update MainFragment.kt

--- app/src/main/java/dam/moviles/runnifydamdaw/vista/MainFragment.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/vista/MainFragment.kt
@@ -5,23 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import dam.moviles.runnifydamdaw.databinding.FragmentMainBinding
+import dam.moviles.runnifydamdaw.modelo.CarreraAdapter
+import dam.moviles.runnifydamdaw.viewModel.MainFragmentViewModel
 
 
 class MainFragment : Fragment() {
 
     lateinit var binding: FragmentMainBinding
+    lateinit var viewModel:MainFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         inicializarBinding(inflater, container)
+        inicializarRecyclerView()
         return binding.root
     }
 
     private fun inicializarBinding(inflater: LayoutInflater, container: ViewGroup?){
         binding=FragmentMainBinding.inflate(inflater,container,false)
+    }
+
+    fun inicializarRecyclerView(){
+        binding.lstCarreras.adapter = CarreraAdapter(viewModel.listaCarreras){ holder ->
+
+
+
+        }
     }
 
 }
