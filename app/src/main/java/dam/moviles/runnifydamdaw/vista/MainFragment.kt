0f483992f798ac3b7424528f95689b6e859bb8d7
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 17:08:06 +0100

    Update MainFragment.kt

--- app/src/main/java/dam/moviles/runnifydamdaw/vista/MainFragment.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/vista/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import dam.moviles.runnifydamdaw.databinding.FragmentMainBinding
 import dam.moviles.runnifydamdaw.modelo.CarreraAdapter
@@ -21,12 +22,17 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         inicializarBinding(inflater, container)
+        inicializarViewModel()
         inicializarRecyclerView()
         return binding.root
     }
 
     private fun inicializarBinding(inflater: LayoutInflater, container: ViewGroup?){
         binding=FragmentMainBinding.inflate(inflater,container,false)
+    }
+
+    private fun inicializarViewModel(){
+        viewModel = ViewModelProvider(this).get(MainFragmentViewModel::class.java)
     }
 
     fun inicializarRecyclerView(){
