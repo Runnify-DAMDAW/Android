2ee031726eb794b8407de5723ac9b603006bd209
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 16:56:41 +0100

    Create MainFragmentViewModel.kt

---
+++ app/src/main/java/dam/moviles/runnifydamdaw/viewModel/MainFragmentViewModel.kt
@@ -0,0 +1,9 @@
+package dam.moviles.runnifydamdaw.viewModel
+
+import androidx.lifecycle.ViewModel
+import dam.moviles.runnifydamdaw.modelo.Carrera
+
+class MainFragmentViewModel: ViewModel() {
+
+    var listaCarreras:List<Carrera> = emptyList()
+}
