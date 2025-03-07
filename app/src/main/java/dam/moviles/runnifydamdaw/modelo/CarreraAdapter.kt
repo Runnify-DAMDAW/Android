41fc09ab7c1d65e13a41ea662803919230dca383
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 16:51:28 +0100

    Update CarreraAdapter.kt

--- app/src/main/java/dam/moviles/runnifydamdaw/modelo/CarreraAdapter.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/modelo/CarreraAdapter.kt
@@ -1,4 +1,29 @@
 package dam.moviles.runnifydamdaw.modelo
 
-class CarreraAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dam.moviles.runnifydamdaw.databinding.ItemcarreraBinding
+
+class CarreraAdapter(
+    var carrera:List<Carrera>,
+    val lambda: (CarreraHolder) -> Unit
+) : RecyclerView.Adapter<CarreraHolder> (){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarreraHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemcarreraBinding.inflate(inflater, parent, false)
+        return CarreraHolder(binding)
+    }
+
+    override fun getItemCount(): Int = carrera.size
+
+    override fun onBindViewHolder(holder: CarreraHolder, position: Int) {
+        val carrera = carrera.get(position)
+        holder.mostrarCarrera(carrera)
+        holder.binding.root.setOnClickListener{
+            lambda(holder)
+        }
+    }
+
 }
