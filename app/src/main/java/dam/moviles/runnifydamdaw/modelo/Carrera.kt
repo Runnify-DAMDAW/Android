f7917481878deb6be5c0eca314c9ecf4b47772ba
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 16:20:48 +0100

    Update Carrera.kt

--- app/src/main/java/dam/moviles/runnifydamdaw/modelo/Carrera.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/modelo/Carrera.kt
@@ -1,3 +1,19 @@
 package dam.moviles.runnifydamdaw.modelo
 
-data class Carrera()
+import java.io.Serializable
+
+data class Carrera(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val date: String,
+    val distance_km: Int,
+    val location: String,
+    val coordinates: String,
+    val entry_fee: Int,
+    val available_slots: Int,
+    val status: String,
+    val category: String,
+    val image: String,
+    val runningParticipants: List<Participante>
+):Serializable
