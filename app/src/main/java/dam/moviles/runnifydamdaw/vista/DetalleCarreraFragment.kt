5f6afd8235ba4064773c0d974511f45734ceff97
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 19:26:13 +0100

    update DetalleCarrera

--- app/src/main/java/dam/moviles/runnifydamdaw/vista/DetalleCarreraFragment.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/vista/DetalleCarreraFragment.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dam.moviles.runnifydamdaw.R
+import dam.moviles.runnifydamdaw.databinding.FragmentDetalleCarreraBinding
 
 class DetalleCarreraFragment : Fragment() {
+
+    lateinit var binding:FragmentDetalleCarreraBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle_carrera, container, false)
+        inicializarBinding(inflater, container)
+        return binding.root
     }
+
+    private fun inicializarBinding(inflater: LayoutInflater, container: ViewGroup?){
+        binding= FragmentDetalleCarreraBinding.inflate(inflater, container, false)
+    }
+
+
 
 }
