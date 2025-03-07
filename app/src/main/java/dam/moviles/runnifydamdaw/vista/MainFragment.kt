7717b4b246c2712f507f9f2e80ce6df6c0155f27
Axlxe05 <158088989+Axlxe05@users.noreply.github.com>
2025-03-07 19:34:06 +0100

    fix MainFragment

--- app/src/main/java/dam/moviles/runnifydamdaw/vista/MainFragment.kt
+++ app/src/main/java/dam/moviles/runnifydamdaw/vista/MainFragment.kt
@@ -52,8 +52,9 @@ class MainFragment : Fragment() {
 
     private fun inicializarRecyclerView() {
         binding.lstCarreras.adapter = CarreraAdapter(viewModel.listaCarreras) { holder ->
-            val carrera = viewModel.listaCarreras.get(holder.adapterPosition)
-            Log.d("MainFragment", "Carrera seleccionada: ${carrera?.name}")
+
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToDetalleCarreraFragment(holder.carrera))
+
         }
     }
 
